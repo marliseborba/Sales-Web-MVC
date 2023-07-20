4f62ff6152197cff41999f4d60ad67b93d729571
marliseborba <borba.marlise@gmail.com>
2023-07-20 08:15:46 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
