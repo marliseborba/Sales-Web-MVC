85e28257fc5c60f5de2f2318f7d205ea00ceb51d
marliseborba <borba.marlise@gmail.com>
2023-07-20 11:02:28 -0300

    MySQL adaptation and first migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -5,6 +5,10 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public Department()
+        {
+        }
+
         public Department(int id, string name)
         {
             Id = id;
