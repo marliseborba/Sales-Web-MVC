8078eaba55f8dfa70ccc5fc5265dff04a1464a1e
marliseborba <borba.marlise@gmail.com>
2023-07-26 11:36:03 -0300

    Update seller and custom service exception

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
+using SalesWebMvc.Services.Exceptions;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -35,6 +36,23 @@ namespace SalesWebMvc.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj) 
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new KeyNotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e) 
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
